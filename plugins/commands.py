35a161ec02b3be1fd3fbe9689fe108bbf055876a
dulquer9 <73124813+dulquer9@users.noreply.github.com>
2021-03-26 15:11:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Creater", url="https://t.me/NickxFury"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Channel List", url="https://t.me/MOVIECLUB_LINKS"),
+                    InlineKeyboardButton("Movie Group", url="https://t.me/MOVIECLUB_chat")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/kurachkanjiedukkatte")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
